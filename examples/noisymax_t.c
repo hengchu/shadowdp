bf3459341022d65cf7885b2f29a28ceb4f54774e
ppht <edgw564731@yahoo.com>
2018-08-01 22:08:45 -0400

    update of transfered program

--- examples/noisymax_t.c
+++ examples/noisymax_t.c
@@ -1,5 +1,3 @@
-/* Implementation of sparse vector
- */
 extern void __VERIFIER_error() __attribute__ ((__noreturn__));
 extern int __VERIFIER_nondet_float(void);
 extern void __VERIFIER_assume(int);
@@ -17,7 +15,7 @@ int noisymax (float epsilon, float q[]) {
 
   __VERIFIER_assume(epsilon > 0);
 
-  int i = 0, bq = 0, s_bq = 0, dis_bq=0;
+  int i = 0, bq = 0, s_bq = 0, dis_bq = 0;
   int dis_s_bq = s_bq - bq;
   float v_epsilon = 0;
 
@@ -26,25 +24,25 @@ int noisymax (float epsilon, float q[]) {
     float dis_q = __VERIFIER_nondet_float();
     __VERIFIER_assume(dis_q >= -1 && dis_q <= 1);
     float eta = __VERIFIER_nondet_float();
-    float s_eta = eta;
+    float s_eta = eta; // maybe identify dis_q in latex
     v_epsilon = ((q[i] + eta > bq || i = 0) ? (abs(1 - dis_q) * (epsilon / 2.0)) : v_epsilon);
 
-    if(q[i] + eta > bq || i = 0)
+    if(q[i] + eta > bq || i == 0)
     {
-      __VERIFIER_assert(q[i] + eta + 1 > bq + dis_s_bq || i = 0);
+      __VERIFIER_assert(q[i] + eta + 1 > bq + dis_s_bq || i == 0);
       int max = i;
       bq = q[i] + eta;
       dis_bq = 1;
     }
     else
     {
-      __VERIFIER_assert(!(q[i] + dis_q + eta > bq + dis_bq || i = 0));
+      __VERIFIER_assert(q[i] + dis_q + eta <= bq + dis_bq); // CPA checker reported line of FALSE
     }
 
     // shadow execution
-    if(q[i] + dis_q + s_eta > s_bq || i = 0)
+    if(q[i] + dis_q + s_eta > s_bq || i == 0)
     {
-      int s_max = i;
+      int s_max = i; // equal in latex
       s_bq = q[i] + dis_q + s_eta;
       dis_s_bq = s_bq - bq;
     }
@@ -67,14 +65,6 @@ int main() {
   unsigned int i;
   for(i = 0; i < SIZE; i++)
     a[i] = __VERIFIER_nondet_float();
-
-  //for(i = 0; i < SIZE; i++)
-  {
-    //da[i] = __VERIFIER_nondet_float();
-    //__VERIFIER_assume(da[i] >= -1 && da[i] <= 1);
-  }
-
-
 
   float epsilon = __VERIFIER_nondet_float();
 
