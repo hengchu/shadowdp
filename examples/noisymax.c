bd068b0b709cc17f3c45a73f18ca4fab3a642042
Ryan Wang <ryanwang.cs@gmail.com>
2018-09-16 16:51:06 -0400

    Add original noisy max program.

---
+++ examples/noisymax.c
@@ -0,0 +1,17 @@
+int noisymax (float epsilon, float q[]) {
+  int i = 0;
+  float bq = 0;
+
+  while(i < SIZE)
+  {
+    float eta = Lap(2 / epsilon);
+    "S_e = q[i] + eta > bq || i == 0 ? SHADOW : ALIGNED; S_eta = q[i] + eta > bq || i == 0 ? ALIGNED : SHADOW; eta : 1"
+
+    if(q[i] + eta > bq || i == 0)
+    {
+      int max = i;
+      bq = q[i] + eta;
+    }
+    i = i + 1;
+  }
+}
