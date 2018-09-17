7cd8c241c5184ccca5a90ae59de446e7a20ec4f5
Ryan Wang <ryanwang.cs@gmail.com>
2018-09-17 19:07:47 -0400

    Use variable functionname.

--- scripts/check.sh
+++ scripts/check.sh
@@ -9,9 +9,9 @@ do
    [ $filename =~ "\/([a-zA-Z][a-zA-Z0-9]*)_t.c" ]
    functionname="${BASH_REMATCH[1]}"
    echo -e "\033[92mChecking \033[1m$filename\033[0m \033[92mwith \033[31;1mMathSat\033[0m"
-   ./cpachecker/scripts/cpa.sh -predicateAnalysis $filename -preprocess -entryfunction functionname
+   ./cpachecker/scripts/cpa.sh -predicateAnalysis $filename -preprocess -entryfunction $functionname
    echo -e "\033[92mChecking \033[1m$filename\033[0m \033[92mwith \033[31;1mSMT-Interpol\033[0m"
-   ./cpachecker/scripts/cpa.sh -predicateAnalysis $filename -preprocess -setprop solver.solver=smtinterpol -entryfunction functionname
+   ./cpachecker/scripts/cpa.sh -predicateAnalysis $filename -preprocess -setprop solver.solver=smtinterpol -entryfunction $functionname
    echo -e "\033[92mChecking \033[1m$filename\033[0m \033[92mwith \033[31;1mZ3\033[0m"
-   ./cpachecker/scripts/cpa.sh -predicateAnalysis $filename -preprocess -setprop solver.solver=z3 -entryfunction functionname
+   ./cpachecker/scripts/cpa.sh -predicateAnalysis $filename -preprocess -setprop solver.solver=z3 -entryfunction $functionname
 done
