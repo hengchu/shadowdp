2216cb6fe5963c87c7aff02d4e66add74776eb14
Ryan Wang <ryanwang.cs@gmail.com>
2018-10-28 16:03:49 -0400

    Add new variables into types dict for Decl Node.

--- lang/core.py
+++ lang/core.py
@@ -46,26 +46,34 @@ class LangTransformer(CGenerator):
         logger.info('Start transforming function {} ...'.format(n.decl.name))
         return super().visit_FuncDef(n)
 
-    def visit_Decl(self, node, no_type=False):
-        decl_type = node.type
+    def visit_Decl(self, n, no_type=False):
+        decl_type = n.type
         if isinstance(decl_type, c_ast.FuncDecl):
             # put parameters into types dict
             for decl in decl_type.args.params:
                 # TODO: fill in the type
                 if isinstance(decl.type, c_ast.TypeDecl):
-                    self._types[decl.name] = decl.type.type.names[0]
+                    self._types[decl.name] = [[0, 0], decl.type.type.names[0]]
                 elif isinstance(decl.type, c_ast.ArrayDecl):
-                    self._types[decl.name] = decl.type.type.type.names[0] + ' array'
-        elif isinstance(decl_type, c_ast.TypeDecl):
+                    self._types[decl.name] = [[0, 0], 'list ' + decl.type.type.type.names[0]]
+            logger.debug('Reserved Params: {}'.format(self._reserved_params))
+        if isinstance(decl_type, c_ast.TypeDecl):
             # put variable declaration into type dict
             # TODO: fill in the type
-            self._types[node.name] = decl_type.type.names[0]
+            if n.init:
+                # TODO: get distance of the expression
+                self._types[n.name] = [[0, 0], decl_type.type.names[0]]
+            else:
+                self._types[n.name] = [[0, 0], decl_type.type.names[0]]
+
+
         elif isinstance(decl_type, c_ast.ArrayDecl):
             # put array variable declaration into type dict
             # TODO: fill in the type
-            self._types[node.name] = decl_type.type.type.names[0] + ' array'
+            self._types[n.name] = [[0, 0], 'list ' + decl_type.type.type.names[0]]
 
-        code = self._code_generator.visit_Decl(node) + ';'
-        logger.info(code)
+        code = self._code_generator.visit_Decl(n)
+        logger.info(code + ';')
         logger.info('types: {}'.format(self._types))
         return code
+
