e1c29ab3ae0618153e69a95d25fe6baa955b8eb9
Ryan Wang <ryanwang.cs@gmail.com>
2018-10-27 21:15:05 -0400

    Print simple message if successfully verified.

--- scripts/check.py
+++ scripts/check.py
@@ -4,19 +4,40 @@ import subprocess
 
 
 def check(path, filename, funcname):
-    print('\033[92mChecking \033[0;1m{}\033[0m \033[92mwith \033[31;1mMathSat\033[0m'.format(filename))
-    subprocess.call(
+    # check with MathSat
+    mathsat = subprocess.Popen(
         ['./cpachecker/scripts/cpa.sh', '-predicateAnalysis', path, '-preprocess', '-entryfunction', funcname,
          '-setprop', 'cpa.predicate.encodeFloatAs=RATIONAL', '-setprop', 'solver.nonLinearArithmetic=USE'])
-    print('\033[92mChecking \033[1m{}\033[0m \033[92mwith \033[31;1mSMT-Interpol\033[0m'.format(filename))
-    subprocess.call(
+    # check with SMT-Interpol
+    smtinterpol = subprocess.Popen(
         ['./cpachecker/scripts/cpa.sh', '-predicateAnalysis-linear', path, '-preprocess', '-entryfunction', funcname,
          '-setprop', 'solver.solver=smtinterpol'])
-    print('\033[92mChecking \033[1m{}\033[0m \033[92mwith \033[31;1mZ3\033[0m'.format(filename))
-    subprocess.call(
+    # check with Z3
+    z3 = subprocess.Popen(
         ['./cpachecker/scripts/cpa.sh', '-predicateAnalysis', path, '-preprocess', '-entryfunction', funcname,
          '-setprop', 'solver.solver=z3', '-setprop', 'cpa.predicate.encodeFloatAs=RATIONAL',
          '-setprop', 'solver.nonLinearArithmetic=USE'])
+
+    outputs = []
+    is_verified = False
+    for name, proc in zip(('SMT-Interpol', 'MathSat', 'Z3'), (smtinterpol, mathsat, z3)):
+        out, err = proc.communicate()
+        outputs.append((out, err))
+        if r'Verification result: TRUE' in out:
+            print('Verified with {}'.format(name))
+            is_verified = True
+            break
+
+    if not is_verified:
+        for name, (out, err) in zip(('SMT-Interpol', 'MathSat', 'Z3'), outputs):
+            print('Error message from {}:'.format(name))
+            print(out)
+            print(err)
+
+    # kill all sub-processes
+    for proc in (smtinterpol, mathsat, z3):
+        proc.kill()
+        proc.wait()
 
 
 if __name__ == '__main__':
