e8c22c014850138fbb6bd60601527c16487002eb
Ryan Wang <ryanwang.cs@gmail.com>
2018-11-01 12:06:56 -0400

    Update transformed program.

--- examples/noisymax_t.c
+++ examples/noisymax_t.c
@@ -11,23 +11,24 @@ typedef enum { false = 0, true = 1 } bool;
 
 
 int noisymax (float epsilon, int size, float q[]) {
-  __VERIFIER_assume(epsilon > 0);
+  __VERIFIER_assume(epsilon >= 0);
   float __LANG_distance_q[size];
   for(int i = 0; i < size; i++)
     __LANG_distance_q[i] = __VERIFIER_nondet_float();
   float __LANG_v_epsilon = 0;
+  int max; float __LANG_distance_max; float __LANG_distance_shadow_max;
 
   int i = 0;
   float bq = 0;
-  float __LANG_shadow_bq = 0, __LANG_distance_bq = 0;
-  float __LANG_distance_shadow_bq = __LANG_shadow_bq - bq;
+  float __LANG_distance_bq = 0;
+  float __LANG_distance_shadow_bq = 0;
 
+  __LANG_distance_bq = 0;
   while(i < size)
   {
     float eta = __VERIFIER_nondet_float();
-    float __LANG_shadow_eta = eta;
     // align by 2
-    __LANG_v_epsilon = (q[i] + eta > bq || i == 0) ? epsilon: __LANG_v_epsilon;
+    __LANG_v_epsilon = (q[i] + eta > bq || i == 0) ? 0 +  2 * (epsilon / 2.0): __LANG_v_epsilon + 0;
 
     if(q[i] + eta > bq || i == 0)
     {
@@ -39,15 +40,14 @@ int noisymax (float epsilon, int size, float q[]) {
     else
     {
       __VERIFIER_assume(__LANG_distance_q[i] <= 1 && -1 <= __LANG_distance_q[i]);
-      __VERIFIER_assert(q[i] + __LANG_distance_q[i] + eta <= bq + __LANG_distance_bq);
+      __VERIFIER_assert(q[i] + __LANG_distance_q[i] + eta + 0 <= bq + __LANG_distance_bq && i != 0);
     }
 
     // shadow execution
-    __LANG_shadow_bq = bq + __LANG_distance_shadow_bq;
-    if(q[i] + __LANG_distance_q[i] + __LANG_shadow_eta > __LANG_shadow_bq || i == 0)
+    if(q[i] + __LANG_distance_q[i] + eta + 0 > bq + __LANG_distance_shadow_bq || i == 0)
     {
-      __LANG_shadow_bq = q[i] + __LANG_distance_q[i] + __LANG_shadow_eta;
-      __LANG_distance_shadow_bq = __LANG_shadow_bq - bq;
+      __LANG_distance_shadow_max = i - max;
+      __LANG_distance_shadow_bq = __LANG_distance_q[i] + 0;
     }
     i = i + 1;
   }
