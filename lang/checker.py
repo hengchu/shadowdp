bfb192c6aa0e6303256d6a2c3d8afd888a396dfb
Ryan Wang <ryanwang.cs@gmail.com>
2018-11-01 23:06:49 -0400

    Merge check function into the lang package.

---
+++ lang/checker.py
@@ -0,0 +1,52 @@
+from collections import OrderedDict
+import subprocess
+import logging
+logger = logging.getLogger(__name__)
+
+
+def check(checkerpath, path, filename, funcname):
+    processes = OrderedDict()
+    processes['MathSat'] = subprocess.Popen(
+        [checkerpath + '/scripts/cpa.sh', '-predicateAnalysis', path, '-preprocess', '-entryfunction', funcname,
+         '-setprop', 'cpa.predicate.encodeFloatAs=RATIONAL', '-setprop', 'solver.nonLinearArithmetic=USE'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    processes['Z3'] = subprocess.Popen(
+        [checkerpath + '/scripts/cpa.sh', '-predicateAnalysis', path, '-preprocess', '-entryfunction', funcname,
+         '-setprop', 'solver.solver=z3', '-setprop', 'cpa.predicate.encodeFloatAs=RATIONAL',
+         '-setprop', 'solver.nonLinearArithmetic=USE'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    processes['SMTInterpol'] = subprocess.Popen(
+        [checkerpath + '/scripts/cpa.sh', '-predicateAnalysis-linear', path, '-preprocess', '-entryfunction', funcname,
+         '-setprop', 'solver.solver=smtinterpol'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    outputs = []
+    is_verified = False
+    for name, proc in processes.items():
+        # wait for 30 seconds
+        try:
+            if not is_verified:
+                out, err = proc.communicate(timeout=30)
+                outputs.append((name, out, err))
+                if r'Verification result: TRUE' in str(out):
+                    print('\033[0;1m{}\033[0m\033[92m verified with {}\033[0m'.format(filename, name))
+                    is_verified = True
+                    break
+        except subprocess.TimeoutExpired:
+            continue
+        finally:
+            proc.kill()
+            proc.wait()
+
+    # if no solvers can verify the program
+    if not is_verified:
+        for name, out, err in outputs:
+            logger.debug('No solvers can verify the program, error messages shown below:')
+            logger.debug('{}:\n\tout: {}\n\t{}'.format(name, out, err))
+
+    return is_verified
--- scripts/check.py
+++ scripts/check.py
@@ -1,55 +1,6 @@
 import os
 import argparse
-import subprocess
-
-
-def check(path, filename, funcname):
-    # check with MathSat
-    mathsat = subprocess.Popen(
-        ['./cpachecker/scripts/cpa.sh', '-predicateAnalysis', path, '-preprocess', '-entryfunction', funcname,
-         '-setprop', 'cpa.predicate.encodeFloatAs=RATIONAL', '-setprop', 'solver.nonLinearArithmetic=USE'],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
-    # check with SMT-Interpol
-    smtinterpol = subprocess.Popen(
-        ['./cpachecker/scripts/cpa.sh', '-predicateAnalysis-linear', path, '-preprocess', '-entryfunction', funcname,
-         '-setprop', 'solver.solver=smtinterpol'],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
-    # check with Z3
-    z3 = subprocess.Popen(
-        ['./cpachecker/scripts/cpa.sh', '-predicateAnalysis', path, '-preprocess', '-entryfunction', funcname,
-         '-setprop', 'solver.solver=z3', '-setprop', 'cpa.predicate.encodeFloatAs=RATIONAL',
-         '-setprop', 'solver.nonLinearArithmetic=USE'],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
-
-    outputs = []
-    is_verified = False
-    for name, proc in zip(('SMT-Interpol', 'MathSat', 'Z3'), (smtinterpol, mathsat, z3)):
-        out, err = proc.communicate()
-        outputs.append((out, err))
-        if r'Verification result: TRUE' in str(out):
-            print('\033[0;1m{}\033[0m\033[92m verified with {}\033[0m'.format(filename, name))
-            is_verified = True
-            break
-
-    if not is_verified:
-        for name, (out, err) in zip(('SMT-Interpol', 'MathSat', 'Z3'), outputs):
-            print('\033[31;1mCan\'t verify \033[0;1m{}\033[0m\033[31;1m, error message from {}:\033[0m'.format(filename, name))
-            print(out)
-            print(err)
-
-    # kill all sub-processes
-    for proc in (smtinterpol, mathsat, z3):
-        proc.kill()
-        proc.wait()
-
-    return is_verified
-
+from lang.checker import check
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Verify the transformed programs.')
@@ -62,7 +13,7 @@ if __name__ == '__main__':
     for root, _, files in os.walk(folder):
         for file in files:
             if file.endswith('_t.c'):
-                if not check(os.path.join(root, file), file, file[:len(file) - 4]):
+                if not check('./cpachecker', os.path.join(root, file), file, file[:len(file) - 4]):
                     all_verified = False
     if all_verified:
         exit(0)
