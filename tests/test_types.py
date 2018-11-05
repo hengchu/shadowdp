bb8e5af6a8a361d490c875d5b71507c8d7aff5f1
Ryan Wang <ryanwang.cs@gmail.com>
2018-11-04 22:32:57 -0500

    Change the tests for TypeSystem.

--- tests/test_types.py
+++ tests/test_types.py
@@ -3,5 +3,5 @@ from lang.types import TypeSystem
 
 def test_type_system():
     types = TypeSystem()
-    types['a'] = ['*', '*']
-    assert types['a'] == ['__LANG_distance_a', '__LANG_distance_shadow_a']
+    types.update_distance('a', '*', '*')
+    assert types.get_distance('a') == ('__LANG_distance_a', '__LANG_distance_shadow_a')
