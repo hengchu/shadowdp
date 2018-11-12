fe16435a0069a784b599e58078074104cbeaa9f6
Ryan Wang <ryanwang.cs@gmail.com>
2018-11-11 23:39:39 -0500

    Add check for sparsevector and partialsum.

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -3,3 +3,5 @@ from lang.checker import check
 
 def test_check():
     assert check('./cpachecker', './examples/noisymax_t.c', 'noisymax')
+    assert check('./cpachecker', './examples/sparsevector_t.c', 'sparsevector')
+    assert check('./cpachecker', './examples/partialsum_t.c', 'partialsum')
