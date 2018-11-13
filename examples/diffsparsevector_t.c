e518ab38240d70a5b00cf8dd5b26febc28a8cf93
Ryan Wang <ryanwang.cs@gmail.com>
2018-11-12 20:34:11 -0500

    Fix parameter issue.

--- examples/diffsparsevector_t.c
+++ examples/diffsparsevector_t.c
@@ -7,9 +7,10 @@ extern void __assert_fail();
 #define abs(x) ((x) < 0 ? -(x) : (x))
 typedef enum { false = 0, true = 1 } bool;
     
-int diffsparsevector(float epsilon, int size, float q[], float T, float __LANG_distance_q)
+int diffsparsevector(float epsilon, int size, float q[], float T, float __LANG_distance_q[])
 {
   __VERIFIER_assume(epsilon >= 0);
+  __VERIFIER_assume(size > 0);
   float __LANG_v_epsilon = 0;
 
   float __LANG_distance_out = 0;
