6bc176638a75f963d74a535df5d621dce8ee014a
Ryan (Yuxin) Wang <ryanwang.cs@gmail.com>
2019-01-14 13:13:58 -0500

    Revert to cpachecker version 1.7

--- scripts/get_cpachecker.sh
+++ scripts/get_cpachecker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget https://cpachecker.sosy-lab.org/CPAchecker-1.8-unix.tar.bz2
-tar xvjf CPAchecker-1.8-unix.tar.bz2
-mv CPAchecker-1.8-unix cpachecker
+wget https://cpachecker.sosy-lab.org/CPAchecker-1.7-unix.tar.bz2
+tar xvjf CPAchecker-1.7-unix.tar.bz2
+mv CPAchecker-1.7-unix cpachecker
