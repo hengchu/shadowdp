41d85bd61e600ee469bd44f44f86b90c4ec4d13d
Ryan Wang <ryanwang.cs@gmail.com>
2019-02-22 13:31:38 -0500

    Fix the type of M.

--- examples/original/smartsum.c
+++ examples/original/smartsum.c
@@ -1,4 +1,4 @@
-void smartsum(float epsilon, int size, float q[], float T, float M)
+void smartsum(float epsilon, int size, float q[], float T, int M)
 {
   float out = 0;
   float next = 0; float n = 0; int i = 0; float sum = 0;
