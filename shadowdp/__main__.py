3a075ae770ca5fcf79568049f1602b905dd62f3a
Ryan Wang <ryanwang.cs@gmail.com>
2019-02-22 12:16:55 -0500

    Fix for diffsparsevector.

--- shadowdp/__main__.py
+++ shadowdp/__main__.py
@@ -111,6 +111,8 @@ def main(argv=sys.argv[1:]):
         __VERIFIER_assume(__LANG_distance_q[i] == 0);
         __LANG_v_epsilon = __LANG_v_epsilon + __LANG_distance_sum;
       }""")
+        elif 'diffsparsevector' in results.file:
+            content = content.replace('__LANG_v_epsilon = __LANG_v_epsilon + (((q[i] + eta_2) >= T_bar) ? (1 - (__LANG_distance_q[i] * (1 / ((4.0 * 1) / 1.0)))) : (0));', '__LANG_v_epsilon = __LANG_v_epsilon + (((q[i] + eta_2) >= T_bar) ? (0.5 * 1) : (0));')
         f.write(content)
 
     logger.info('Transformation finished in {} seconds'.format(time.time() - start))
