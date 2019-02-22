c2dce49c6e44654d20928a83e48020eaaaec32ec
Ryan Wang <ryanwang.cs@gmail.com>
2019-02-22 14:09:40 -0500

    Print new line after each test.

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 shadowdp examples/original/noisymax.c
+echo ''
 shadowdp examples/original/sparsevector.c
+echo ''
 # apply epsilon = 1 technique to solve non-linearity
 shadowdp examples/original/diffsparsevector.c -e
+echo ''
 shadowdp examples/original/partialsum.c -e
+echo ''
 shadowdp examples/original/smartsum.c -e
