23c47f3798a1420bbd67f21a81f1e5057ef100d3
Yuxin (Ryan) Wang <ryanwang.cs@gmail.com>
2019-03-19 17:31:30 -0400

    Add SparseVectorN.

---
+++ examples/original/sparsevectorN.c
@@ -0,0 +1,27 @@
+int sparsevectorN(float epsilon, int size, float q[], float T, int NN)
+{
+  "ALL_DIFFER; epsilon: <0, 0>; size: <0, 0>; q: <*, *>; T: <0, 0>; NN: <0, 0>";
+  int out = 0;
+  float eta_1 = Lap(2.0 / epsilon, "ALIGNED; 1;");
+  float T_bar = T + eta_1;
+  int c_1 = 0, c_2 = 0;
+  int i = 0;
+
+  while (c_1 < NN && i < size)
+  {
+    float eta_2 = Lap((4.0 * NN / epsilon), "ALIGNED; (q[i] + eta_2 >= T_bar) ? 2 : 0;");
+
+    if (q[i] + eta_2 >= T_bar)
+    {
+      out = 1;
+      c_1 = c_1 + 1;
+    }
+    else
+    {
+      out = 0;
+      c_2 = c_2 + 1;
+    }
+    i = i + 1;
+  }
+  return out;
+}
