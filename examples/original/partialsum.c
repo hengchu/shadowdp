b06cc4d05f9e30897153e4aa82fa9a186bc5dd51
Yuxin (Ryan) Wang <ryanwang.cs@gmail.com>
2019-03-19 01:00:24 -0400

    Fix annotation for partialsum and smartsum.

--- examples/original/partialsum.c
+++ examples/original/partialsum.c
@@ -8,7 +8,7 @@ int partialsum (float epsilon, int size, float q[])
     sum = sum + q[i];
     i = i + 1;
   }
-  float eta = Lap(1.0 / epsilon, "ALIGNED; __SHADOWDP_ALIGNED_sum - sum;");
+  float eta = Lap(1.0 / epsilon, "ALIGNED; -(__SHADOWDP_ALIGNED_sum - sum);");
   out = sum + eta;
   return out;
 }
